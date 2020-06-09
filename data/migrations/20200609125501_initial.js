fdcef8f5018773cbc4c5429776cce54422b8e99b
William Schwindt <williammschwindt@gmail.com>
2020-06-09 13:45:09 -0700

    schema created

---
+++ data/migrations/20200609125501_initial.js
@@ -0,0 +1,43 @@
+
+exports.up = async function(knex) {
+    await knex.schema.createTable('users', (table) => {
+        table.increments('id')
+        table.text('username').notNull().unique()
+        table.text('password').notNull()
+        table.text('name').notNull()
+        table.text('address').notNull()
+    })
+
+    await knex.schema.createTable('stores', (table) => {
+        table.increments('id')
+        table.text('store-name').notNull()
+        table.text('store-address').notNull()
+    })
+
+    await knex.schema.createTable('items', (table) => {
+        table.increments('id')
+        table.text('name').notNull()
+        table.integer('price').notNull()
+    })
+
+    await knex.schema.createTable('stores_items', (table) => {
+        table.integer('store_id')
+            .references('id')
+            .inTable('stores')
+            .onDelete('CASCADE')
+            .onUpdate('CASCADE')
+        table.integer('item_id')
+            .references('id')
+            .inTable('items')
+            .onDelete('CASCADE')
+            .onUpdate('CASCADE')
+        table.primary(['store_id', 'item_id'])
+    })
+};
+
+exports.down = async function(knex) {
+    await knex.schema.dropTableIfExists('stores_items')
+    await knex.schema.dropTableIfExists('items')
+    await knex.schema.dropTableIfExists('stores')
+    await knex.schema.dropTableIfExists('users')
+};
