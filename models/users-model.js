553f71f737d2420133e39d8133c825fc501444b2
William Schwindt <williammschwindt@gmail.com>
2020-06-11 12:40:27 -0700

    users model

---
+++ models/users-model.js
@@ -0,0 +1,33 @@
+const db = require('../data/config')
+const bcrypt = require('bcryptjs')
+
+function findUsers() {
+    return db('users')
+}
+
+function findById(id) {
+    return db('users').where({id}).first()
+}
+
+async function addUser(user) {
+    user.password = await bcrypt.hash(user.password, 12)
+    const [id] = await db('users').insert(user).returning('id')
+    return await findById(id)
+}
+
+async function updateUser(id, user) {
+    await db('users').where({id}).update(user)
+    return findById(id)
+}
+
+function deleteUser(id) {
+    db('users').where({id}).del()
+}
+
+module.exports = {
+    findUsers,
+    findById,
+    addUser,
+    updateUser,
+    deleteUser
+}
